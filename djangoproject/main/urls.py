cb665aa1101b89f4c062ce8ce402eaf83b2553c9
Ramzan Gadamurov <79451112+D10@users.noreply.github.com>
2021-05-13 01:48:42 +0200

    Version 2

--- djangoproject/main/urls.py
+++
@@ -1,8 +0,0 @@
-# Файл приложения main, в котором обозначены url адреса всех страниц этого веб приложения
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('about', views.about, name='about')
-]
--- djangoproject/news/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Articles
-
-# Регистрация БД для управления ей из панели администратора
-admin.site.register(Articles)
