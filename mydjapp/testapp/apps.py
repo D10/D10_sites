64ab872efe1dd026bcc4c8587f21f263854333e8
Ramzan <rgadamurov@bk.ru>
2021-05-15 20:58:50 +0200

    mydjapp
    release v6

--- mydjapp/testapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestappConfig(AppConfig):
-    name = 'testapp'
-    verbose_name = 'Новости'  # Отображения в панели админа как название приложения
--- mydjapp/testtest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TesttestConfig(AppConfig):
-    name = 'testtest'
